7055591ff8d4acf0da672c5259f0dfa2db0b1403
Hannah Malm <hannahemalm616@gmail.com>
2020-12-12 17:52:08 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,4 +11,7 @@ def happy_birthday(birthday_kids)
 end
 
 
-
+# code needs to puts out to the terminal the following message: "Happy Birthday #{kids_name}! You are now #{age} years old!"
+#use each to iterate over each pair of kids name/age 
+#yield the key/value pair to the block of code between the do end keywords by assigning the variables kids_name and age in between pipes 
+#Use those variable names in string interpolation to puts out the actual values they point to at each step of the iteration 
